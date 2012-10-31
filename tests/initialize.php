b08405b1eeda6bf13402d5a8a1d95837b9fdddb6
Remi JANOT <r.janot@gmail.com>
2012-10-31 14:42:37 +0100

    [TEST] Fix bootstrap

--- tests/initialize.php
+++ tests/initialize.php
@@ -9,14 +9,14 @@
  */
 
 // This is included first by PHPUnit to initialize autoloader
-require_once __DIR__.'/../../symfony/src/Symfony/Component/ClassLoader/UniversalClassLoader.php';
+require_once __DIR__.'/../../../symfony/symfony/src/Symfony/Component/ClassLoader/UniversalClassLoader.php';
 use Symfony\Component\ClassLoader\UniversalClassLoader;
 
 $loader = new UniversalClassLoader();
 $loader->registerNamespaces(array(
-    'Doctrine\\Common' => __DIR__.'/../../doctrine-common/lib',
-    'Doctrine\\DBAL'   => __DIR__.'/../../doctrine-dbal/lib',
-    'Yucca\\Concrete'  => __DIR__.'/../tests',
+    'Doctrine\\Common' => __DIR__.'/../../../doctrine/common/lib',
+    'Doctrine\\DBAL'   => __DIR__.'/../../../doctrine/dbal/lib',
+    'Yucca\\Concrete'  => __DIR__,
     'Yucca'            => __DIR__.'/../lib',
 ));
 $loader->register();
